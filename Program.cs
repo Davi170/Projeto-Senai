120b49250a71169133a965adc1abbe2ea0e7af61
Davi170 <103954717+Davi170@users.noreply.github.com>
2022-08-01 17:22:34 -0300

    Ferramentas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Projeto_Senai.Model;
+
+ExemploSenaiContext context = new ExemploSenaiContext();
+while (true)
+{
+  Console.WriteLine("O que deseja fazer? 1 - Logar ou 2 - Registrar ou 3 - Sair");
+  string num = Console. ReadLine();
+  if (num == "1")
+  {
+     Console.Clear();
+  }
+  else if (num == "2")
+  {
+     Console.Clear();
+  }
+  else if (num == "3")
+  {
+     break;
+  }
+  else
+  {
+     Console.Clear();
+     Console.WriteLine("Operação Invalida");
+  }
+
+}
