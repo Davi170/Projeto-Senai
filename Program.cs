5c465898c39052e586800b337c4a780186de1af5
Davi170 <103954717+Davi170@users.noreply.github.com>
2022-08-03 15:44:51 -0300

    registro de Pessoas

--- Program.cs
+++ Program.cs
@@ -4,14 +4,39 @@ ExemploSenaiContext context = new ExemploSenaiContext();
 while (true)
 {
   Console.WriteLine("O que deseja fazer? 1 - Logar ou 2 - Registrar ou 3 - Sair");
-  string num = Console. ReadLine();
+  string num = Console.ReadLine();
   if (num == "1")
   {
-     Console.Clear();
+     Console.WriteLine("Digite seu nome:");
+     string nome = Console.ReadLine();
+
+     Console.WriteLine("Digite a sua Senha:");
+     string Senha = Console.ReadLine();
+
+
+     Usuario expectedUser = null;
+   foreach (Usuario usuario in db.Usuarios)
+   {
+     {
+        expectedUser = usuario;
+     }
+   }
+     if (expectedUser == null)
+     {
+        Console.WriteLine("Não existe usuário com esse nome");
+        Console.ReadKey(true);
+        Console.Clear();
+     }
   }
   else if (num == "2")
   {
-     Console.Clear();
+     Usuario usuario = new Usuario();
+
+     Console.WriteLine("Digite seu nome:");
+     usuario.Nome = Console.ReadLine();
+
+     Console.WriteLine("Digite a sua Senha:");
+     usuario.Nome = Console.ReadLine();
   }
   else if (num == "3")
   {
